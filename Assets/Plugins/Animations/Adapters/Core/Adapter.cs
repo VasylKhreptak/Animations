0684f41885baa7c300624c78da030128068b4797
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-10-02 09:29:36 +0300

    Updated Adapter.cs;

--- Assets/Plugins/Animations/Adapters/Core/Adapter.cs
+++ Assets/Plugins/Animations/Adapters/Core/Adapter.cs
@@ -5,14 +5,5 @@ namespace Plugins.Animations.Adapters.Core
     public abstract class Adapter<T> : MonoBehaviour
     {
         public abstract T Value { get; set; }
-
-        #region MonoBehaviour
-
-        private void OnValidate()
-        {
-            Value ??= GetComponent<T>();
-        }
-
-        #endregion
     }
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,6 +8,6 @@ public class Test : MonoBehaviour
 
     private void Awake()
     {
-        
+
     }
 }
